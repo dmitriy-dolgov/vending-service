fcdf978925736b034dc03ff5141d58cae4b59108
dmitriy-dolgov <TwilightTower@mail.ru>
2020-05-20 22:14:22 +0300

    api stuff

--- lib/Api.php
+++ lib/Api.php
@@ -4,11 +4,47 @@ namespace lib;
 
 class Api
 {
+    const API_URL = 'http://178.57.218.210:4001/';
+
     // для пароля test/test1
     protected $apiKey = 'TS3qVh70xrM59VC9OxqK3UZV';
 
-    public function command()
-    {
 
+    public function command(string $method, string $path, array $query = [])
+    {
+        switch ($method) {
+            case 'get':
+            {
+                $result = $this->cmdGet($path, $query);
+                break;
+            }
+            case 'post':
+            {
+                break;
+            }
+            default:
+            {
+                throw new \Exception('Wrong method: ' . $method);
+            }
+        }
+
+        return $result;
+    }
+
+    protected function cmdGet(string $path, array $query = [])
+    {
+        $result = false;
+
+        $url = self::API_URL . $path;
+
+        if ($query) {
+            $url .= '?' . http_build_query($query);
+        }
+
+        if ($resultRaw = file_get_contents($url)) {
+            $result = json_decode($resultRaw, true);
+        }
+
+        return $result;
     }
 }
--- lib/items/Equipment.php
+++ lib/items/Equipment.php
@@ -2,22 +2,32 @@
 
 namespace lib\items;
 
+use lib\Api;
 use lib\ItemCache;
 
 class Equipment extends ItemCache
 {
+    protected $api;
+
+
     protected function getUniqueName()
     {
         return 'equipment';
     }
 
+    public function __construct()
+    {
+        $this->api = new Api();
+    }
+
     protected function getItemsFromPrimaryRepository()
     {
-        Api::command();
+        return Api::command('get', 'machines');
     }
 
     public function getList()
     {
-        //public function
+        $items = parent::getItems();
+        return $items;
     }
 }
