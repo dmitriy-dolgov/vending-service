d0cdbf3f35ce2359c5e81d1ae2913b19887e2d34
dmitriy-dolgov <TwilightTower@mail.ru>
2020-05-21 22:24:29 +0300

    top menu html, css stuff

--- html/header.php
+++ html/header.php
@@ -35,9 +35,12 @@ require APP_DIR . 'vendor/autoload.php';
     <title></title>
     <base href="/">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.standalone.min.css"/>
-    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.standalone.min.css"/>
+    <link rel="stylesheet" type="text/css"
+          href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
@@ -47,28 +50,21 @@ require APP_DIR . 'vendor/autoload.php';
 </head>
 <body>
 
-    <nav class="navbar navbar-light bg-primary navbar-expand-sm">
-        <div class="container">
-            <div class="d-flex align-items-center"><a
-                        class="navbar-brand logo navbar-link"
-                        routerlink="/" ng-reflect-router-link="/"
-                        href="/">Сервис вендинга</a></div>
-            <div class="d-flex align-items-center"><a class="sign-in-btn navbar-link ng-star-inserted"><span
-                            class="d-none d-sm-inline">Выйти</span>
-                    <app-icon class="app-icon" name="sign-out" size="24" _nghost-mlo-c1=""
-                              ng-reflect-size="24" ng-reflect-name="sign-out"
-                              style="background-image: url(&quot;sign-out.png&quot;); min-height: 24px; min-width: 24px; height: 24px; width: 24px;"></app-icon>
-                </a>
-                <button aria-haspopup="true" aria-label="Toggle navigation"
-                        class="btn btn btn-sm navbar-toggler-btn" type="button" ng-reflect-menu="[object Object]">
-                    <app-icon class="app-icon" name="ic_header_menu" _nghost-mlo-c1=""
-                              ng-reflect-size="24" ng-reflect-name="ic_header_menu"
-                              style="background-image: url(&quot;ic_header_menu.png&quot;); min-height: 24px; min-width: 24px; height: 24px; width: 24px;"></app-icon>
-                </button>
-                <mat-menu class="" xposition="before" ng-reflect-x-position="before"
-                          ng-reflect-panel-class="base-nav-menu"></mat-menu>
-            </div>
+<nav class="navbar navbar-light bg-primary navbar-expand-sm">
+    <div class="container">
+        <div class="d-flex align-items-center">
+            <a class="navbar-brand logo navbar-link" href="/">Сервис вендинга</a>
         </div>
-    </nav>
+        <div class="d-flex align-items-center">
+            <a class="sign-in-btn navbar-link ng-star-inserted">
+                <span class="d-sm-inline">Выйти</span>
+                <app-icon class="app-icon"></app-icon>
+            </a>
+            <button aria-haspopup="true" aria-label="Toggle navigation"
+                    class="btn btn btn-sm navbar-toggler-btn" type="button">
+            </button>
+        </div>
+    </div>
+</nav>
 
-    <div  class="container">
+<div class="container">
