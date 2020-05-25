6394019a55b5514bbfa11a52e8131e4ad4ab567e
dmitriy-dolgov <TwilightTower@mail.ru>
2020-05-25 14:34:37 +0300

    class autoload extended

--- html/header.php
+++ html/header.php
@@ -23,7 +23,18 @@ set_include_path(get_include_path() . PATH_SEPARATOR . realpath(__DIR__ . '/../'
 //echo get_include_path();exit;
 
 // PSR-0
-spl_autoload_register();
+spl_autoload_register(function ($className) {
+    $className = ltrim($className, '\\');
+    $fileName = '';
+    if ($lastNsPos = strripos($className, '\\')) {
+        $namespace = substr($className, 0, $lastNsPos);
+        $className = substr($className, $lastNsPos + 1);
+        $fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+    }
+    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+
+    require $fileName;
+});
 
 require APP_DIR . 'vendor/autoload.php';
 
